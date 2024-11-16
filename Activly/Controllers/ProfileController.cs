cefd19a99c57a3d2e4b83bd730df5ff6de1d6d08
RollingBass <149956405+RollingBass@users.noreply.github.com>
2024-11-16 18:29:15 +0100

    profil kontroler

---
+++ Activly/Controllers/ProfileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ActivelyServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly string _uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
+        public ProfileController()
+        {
+            if (!Directory.Exists(_uploadsFolder))
+            {
+                Directory.CreateDirectory(_uploadsFolder);
+            }
+        }
+
+        
+    }
+}
