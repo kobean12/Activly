9d8e85fce9848fc22b229d96578771615726a9e0
RollingBass <149956405+RollingBass@users.noreply.github.com>
2025-01-14 22:26:22 +0100

    Delete Activly directory

--- Activly/Models/Class.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Activly2.Models
-{
-    public class PointsModel
-    {
-        public string Points { get; set; }
-    }
-}
--- Activly/Models/UserProfile.cs
+++
@@ -1,6 +0,0 @@
-public class UserProfile
-{
-    public string Gender { get; set; }
-    public int Age { get; set; }
-    public string ProfilePicturePath { get; set; }
-}
